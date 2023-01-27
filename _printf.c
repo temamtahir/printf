a70d30e452b8d364d7a13959eb7ba47530ad489c
Hiwot Berhanu <117803059+BHiwot@users.noreply.github.com>
2023-01-27 09:07:09 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -47,12 +47,9 @@ int _printf(const char *format, ...)
 	return (printed_chars);
 }
 /**
- * * print_buffer - Prints the contents of the buffer if it exist
- *
- * * @buffer: Array of chars
- *
- * * @buff_ind: Index at which to add next char, represents the length.
- *
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
