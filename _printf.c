85b7b7f02ddac0db55cc36613b4f33e7e1e7ddbc
Hiwot Berhanu <117803059+BHiwot@users.noreply.github.com>
2023-01-27 08:51:38 +0300

    Update _printf

--- _printf.c
+++ _printf.c
@@ -1,12 +1,9 @@
 #include "main.h"
 void print_buffer(char buffer[], int *buff_ind);
 /**
- * * _printf - Printf function
- *
- * * @format: format.
- *
- * * Return: Printed chars.
- *
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
