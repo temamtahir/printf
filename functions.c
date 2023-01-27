3ef47808a5944df778769ba5e80af9f22ef3c12d
Hiwot Berhanu <117803059+BHiwot@users.noreply.github.com>
2023-01-27 09:00:45 +0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -74,21 +74,14 @@ int print_string(va_list types, char buffer[],
 }
 /************************* PRINT PERCENT SIGN *************************/
 /**
- * * print_percent - Prints a percent sign
- *
- * * @types: Lista of arguments
- *
- * * @buffer: Buffer array to handle print
- *
- * * @flags:  Calculates active flags
- *
- * * @width: get width.
- *
- * * @precision: Precision specification
- *
- * * @size: Size specifier
- *
- * * Return: Number of chars printed
+ * print_percent - Prints a percent sign
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
  */
 int print_percent(va_list types, char buffer[],
 		int flags, int width, int precision, int size)
@@ -103,21 +96,14 @@ int print_percent(va_list types, char buffer[],
 }
 /************************* PRINT INT *************************/
 /**
- * * print_int - Print int
- *
- * * @types: Lista of arguments
- *
- * * @buffer: Buffer array to handle print
- *
- * * @flags:  Calculates active flags
- *
- * * @width: get width.
- *
- * * @precision: Precision specification
- *
- * * @size: Size specifier
- *
- * * Return: Number of chars printed
+ * print_int - Print int
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
  */
 int print_int(va_list types, char buffer[],
 		int flags, int width, int precision, int size)
@@ -154,22 +140,14 @@ int print_int(va_list types, char buffer[],
 /************************* PRINT BINARY *************************/
 
 /**
- * * print_binary - Prints an unsigned number
- *
- * * @types: Lista of arguments
- *
- * * @buffer: Buffer array to handle print
- *
- * * @flags:  Calculates active flags
- *
- * * @width: get width.
- *
- * * @precision: Precision specification
- *
- * * @size: Size specifier
- *
- * * Return: Numbers of char printed.
- *
+ * print_binary - Prints an unsigned number
+ * types: Lista of arguments
+ * buffer: Buffer array to handle print
+ * flags:  Calculates active flags
+ * width: get width.
+ * precision: Precision specification
+ * size: Size specifier
+ * Return: Numbers of char printed.
  */
 int print_binary(va_list types, char buffer[],
 		int flags, int width, int precision, int size)
